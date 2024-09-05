5100ddbbd8efd0b1ba58f450e3a090b256d704f8
bean <125648438+beanpython1@users.noreply.github.com>
2024-09-05 08:49:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 require('dotenv').config(); // Load environment variables
 
-const fetch = require('node-fetch');
+(async () => {
+  const fetch = (await import('node-fetch')).default;
 
-// Access environment variables
-const airtableApiKey = process.env.AIRTABLE_API_KEY;
-const baseId = process.env.BASE_ID;
-const tableName = process.env.TABLE_NAME;
+  const airtableApiKey = process.env.AIRTABLE_API_KEY;
+  const baseId = process.env.BASE_ID;
+  const tableName = process.env.TABLE_NAME;
 
-async function deleteMessages() {
-    const url = `https://api.airtable.com/v0/${baseId}/${tableName}`;
-    
-    try {
-        const response = await fetch(url, {
-            headers: { 'Authorization': `Bearer ${airtableApiKey}` }
-        });
-        const data = await response.json();
-        
-        const deletePromises = data.records.map(record => {
-            return fetch(`https://api.airtable.com/v0/${baseId}/${tableName}/${record.id}`, {
-                method: 'DELETE',
-                headers: {
-                    'Authorization': `Bearer ${airtableApiKey}`
-                }
-            });
-        });
-        
-        await Promise.all(deletePromises);
-        console.log('Messages deleted from Airtable');
-    } catch (error) {
-        console.error('Error deleting messages:', error);
-    }
-}
+  async function deleteMessages() {
+      const url = `https://api.airtable.com/v0/${baseId}/${tableName}`;
+      
+      try {
+          const response = await fetch(url, {
+              headers: { 'Authorization': `Bearer ${airtableApiKey}` }
+          });
+          const data = await response.json();
+          
+          const deletePromises = data.records.map(record => {
+              return fetch(`https://api.airtable.com/v0/${baseId}/${tableName}/${record.id}`, {
+                  method: 'DELETE',
+                  headers: {
+                      'Authorization': `Bearer ${airtableApiKey}`
+                  }
+              });
+          });
+          
+          await Promise.all(deletePromises);
+          console.log('Messages deleted from Airtable');
+      } catch (error) {
+          console.error('Error deleting messages:', error);
+      }
+  }
 
-// Schedule the function to run every 12 hours
-const interval = 1000; // 12 hours in milliseconds
-setInterval(deleteMessages, interval);
+  // Schedule the function to run every 12 hours
+  const interval = 1000; // 12 hours in milliseconds
+  setInterval(deleteMessages, interval);
 
-// Run immediately on script start
-deleteMessages();
+  // Run immediately on script start
+  deleteMessages();
+})();
