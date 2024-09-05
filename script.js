3d534bcaef8a56aacca7308ecec41ff4741ae670
bean <125648438+beanpython1@users.noreply.github.com>
2024-09-05 08:48:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ async function deleteMessages() {
 }
 
 // Schedule the function to run every 12 hours
-const interval = 12 * 60 * 60 * 1000; // 12 hours in milliseconds
+const interval = 1000; // 12 hours in milliseconds
 setInterval(deleteMessages, interval);
 
 // Run immediately on script start
